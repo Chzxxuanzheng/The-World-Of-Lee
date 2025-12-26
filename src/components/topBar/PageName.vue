d5c8fc2f44f056a72d2075a1df91764dc1b226ea
Mr.Lee <chzxxuanzheng@gmail.com>
2025-12-27 00:31:20 +0800

    feat: 在主页时有单独的副标题

--- src/components/topBar/PageName.vue
+++ src/components/topBar/PageName.vue
@@ -1,10 +1,21 @@
 <template>
 	<Transition name="page-name" mode="out-in">
-		<span :key="1" class="page-name">{{ page.description }}</span>
+		<span :key="1" class="page-name">{{ name }}</span>
 	</Transition>
 </template>
 <script setup lang="ts">
 import PageController from '@/function/pageController'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const name = computed(()=>{
+	if (route.path === '/') {
+		return 'Welcome To The World Of Lee'
+	}
+	return page.value.description
+})
 
 const page = PageController.useCurrentPage()
 </script>
